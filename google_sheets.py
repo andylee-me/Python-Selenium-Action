3f315ef16eed3344cbab4f11b0323fa689d02d03
andylee-me <60034043+andylee-me@users.noreply.github.com>
2024-10-27 22:21:15 +0800

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -2,6 +2,6 @@ import pygsheets
 
 gc = pygsheets.authorize(service_account_file='path/to/key.json')
 
-survey_url = 'https://docs.google.com/spreadsheets/d/1cNqBIzF_T8yZm5S1hj7E1MNVXQegVDVuJsUWVoDKIDw/'
+survey_url = 'https://docs.google.com/spreadsheets/d/1LVt17F4_7z_8gEz9DR51eBI4mNLxfiFnIcgCuHRe1D4/'
 sh = gc.open_by_url(survey_url)
 print(sh)
