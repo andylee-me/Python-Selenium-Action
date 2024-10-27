ed10bd709c61c7f2f6ea38d400afb069811839b5
andylee-me <60034043+andylee-me@users.noreply.github.com>
2024-10-27 22:48:02 +0800

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,20 +1,6 @@
 import pygsheets
-import pandas as pd
-#authorization
-gc = pygsheets.authorize(service_file='{C:\Users\andy8\Downloads\serious-unison-439914-c7-695430c44d35.json}')
 
-# Create empty dataframe
-df = pd.DataFrame()
+gc = pygsheets.authorize(service_account_file='path/to/key.json')
 
-# Create a column
-df['name'] = ['tao1', 'tao2', 'tao3', "tao4"]
-df['age'] = [11, 12, 13, 14]
-
-#open the google spreadsheet 
-sh = gc.open('{123}')
-
-#select the first worksheet 
-wks = sh[0]  
-
-#update the first worksheet with df, starting at cell A1 i.e.(0,0) 
-wks.set_dataframe(df,(0,0))
+survey_url = 'https://docs.google.com/spreadsheets/d/1LVt17F4_7z_8gEz9DR51eBI4mNLxfiFnIcgCuHRe1D4/'
+sh = gc.open_by_url(survey_url)
