e175d33c8e001f812eaa8cedc35a34d5b6701001
andylee-me <60034043+andylee-me@users.noreply.github.com>
2024-10-28 19:42:10 +0800

    Update Selenium-Template-later.py

--- Selenium-Template-later.py
+++ Selenium-Template-later.py
@@ -59,11 +59,11 @@ time.sleep(2)
 
 #read google-sheets
 url = "https://raw.githubusercontent.com/andylee-me/Python-Selenium-Action/main/%E7%AB%B6%E5%83%B9%E6%8B%8D%E8%B3%A3.csv"
-code = pd.read_csv(url, index_col=1)
+code = pd.read_csv(url)
 code = list(code)
 print("\n\nthisis code",code,"\n\n")
-print("\n\n",code[0],"\n\n")
-  
+print("\n\n",code[1][0],"\n\n")
+
   #if month...
 
 element = driver.find_element(by=By.CLASS_NAME, value="code")
